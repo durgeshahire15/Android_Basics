eaab5ca155c8649dd11da6a49aa7c3c0b03331dd
Durgesh Ahire <durgeshahire15@gmail.com>
2024-12-06 11:00:13 +0530

    Greeting name updated

--- app/src/main/java/com/example/androidproject/MainActivity.kt
+++ app/src/main/java/com/example/androidproject/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             AndroidProjectTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Durgesh Ahire",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
